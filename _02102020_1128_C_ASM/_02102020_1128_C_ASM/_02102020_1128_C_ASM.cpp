a2eb141d83a76fa4121269310bfe13ead31bee73
IS <ivan.sibirev@yandex.ru>
2020-10-02 14:16:12 +0300

    ***

--- _02102020_1128_C_ASM/_02102020_1128_C_ASM/_02102020_1128_C_ASM.cpp
+++ _02102020_1128_C_ASM/_02102020_1128_C_ASM/_02102020_1128_C_ASM.cpp
@@ -2,105 +2,132 @@
 #include <iostream>
 ////////////////////////////////////////////////////////////////////////////////////
 #include<windows.h>
-#define __Set_Test if(true)//
-#define __Set_Test if(false)
-//#define __Set_Test if(true) - на случай тестирования на скорость
-//#define __Set_Test if(false) - на случай отладки...
 #define __Set_TimeTestX 100000000
 #define __Set_TestTimeStart double TickStart = GetTickCount(); for(int i=0;i<__Set_TimeTestX;i++)
 #define __Set_TestTimeStop double TickFinish = GetTickCount();std::cout << "_TimeTest=" << TickFinish-TickStart << std::endl;
 ////////////////////////////////////////////////////////////////////////////////////
-/// <summary>Пример программы с асембленрной вставкой. Обмен значениями двух переменных через ASM вставку.</summary>
-void Test_1()
-{
-    __Set_Test std::cout << "Test_1();" << std::endl;
-    int x = 10;
-    int y = 11;
-    __Set_Test std::cout << "<x,y>=" << "<" << x << "," << y << ">" << std::endl;
-    __Set_TestTimeStart
-    __asm
-    {
-        //Обнуление регистра на пожарный случай
-        xor eax, eax
-        xor ebx, ebx
-        //Команда пересылки данных. eax = x
-        mov eax, dword ptr[x]
-        mov ebx, dword ptr[y]
-        //Команда пересылки данных. x=eax
-        mov dword ptr[x], eax
-        mov dword ptr[y], ebx
-    }
-    __Set_TestTimeStop
-    __Set_Test std::cout << "<x,y>=" << "<" << x << "," << y << ">" << std::endl;
-}
-/// <summary>Пример программы с асембленрной вставкой. Обмен значениями двух переменных через ASM вставку.</summary>
-void Test_2()
-{
-    __Set_Test std::cout << "Test_2();" << std::endl;
-    int x = 10;
-    int y = 11;
-    __Set_Test std::cout << "<x,y>=" << "<" << x << "," << y << ">" << std::endl;
-    __Set_TestTimeStart
-    __asm
-    {
-        xor eax, eax
-        xor ebx, ebx
-        mov eax, dword ptr[x]
-        mov ebx, dword ptr[y]
-        //Обмен значениями регистров
-        XCHG eax,ebx
-        mov dword ptr[x], eax
-        mov dword ptr[y], ebx
-    }
-    __Set_TestTimeStop
-    __Set_Test
-    std::cout << "<x,y>=" << "<" << x << "," << y << ">" << std::endl;
-}
-/// <summary>Модифицированный обмен значениями переменных через XCHG, в 3 строки</summary>
-void Test_3()
-{
-    __Set_Test std::cout << "Test_3();" << std::endl;
-    int x = 10;
-    int y = 11;
-    __Set_Test std::cout << "<x,y>=" << "<" << x << "," << y << ">" << std::endl;
-    __Set_TestTimeStart
-    __asm
-    {
-        xor ebx, ebx
-        mov ebx, dword ptr[x]
-        XCHG ebx, dword ptr[y]
-        mov dword ptr[x], ebx
-    }
-    __Set_TestTimeStop
-    __Set_Test std::cout << "<x,y>=" << "<" << x << "," << y << ">" << std::endl;
-}
-/// <summary>Короткий обмен значениями без приблуд...</summary>
-void Test_4()
-{
-    __Set_Test std::cout << "Test_3();" << std::endl;
-    int x = 10;
-    int y = 11;
-    __Set_Test std::cout << "<x,y>=" << "<" << x << "," << y << ">" << std::endl;
-    __Set_TestTimeStart
-    __asm
-    {
-        xor ebx, ebx
-        mov ebx, x
-        XCHG ebx, y
-        mov x, ebx
-    }
-    __Set_TestTimeStop
-    __Set_Test std::cout << "<x,y>=" << "<" << x << "," << y << ">" << std::endl;
-}
 
 int main()
 {
-    Test_1();
-    Test_2();
-    Test_3();
-    Test_4();
-    //Как видно из примера самый быстрый тест у первой ассемблерной встваки с приблудами от MASM
-    Test_1();
+    /*Test1*/if (false)
+    {
+        int x = 10;int y = 11;
+        std::cout << "<x,y>=" << "<" << x << "," << y << ">" << std::endl;
+        __asm
+        {
+            //Обнуление регистра на пожарный случай
+            xor eax, eax
+            xor ebx, ebx
+            //Команда пересылки данных. eax = x
+            mov eax, dword ptr[x]
+            mov ebx, dword ptr[y]
+            //Команда пересылки данных. x=eax
+            mov dword ptr[x], eax
+            mov dword ptr[y], ebx
+        }
+        std::cout << "<x,y>=" << "<" << x << "," << y << ">" << std::endl;
+    }
+    /*Test2*/else if (false)
+    {
+        int x = 10;int y = 11;
+        std::cout << "<x,y>=" << "<" << x << "," << y << ">" << std::endl;
+        __asm
+        {
+            xor eax, eax
+            xor ebx, ebx
+            mov eax, dword ptr[x]
+            mov ebx, dword ptr[y]
+            //Обмен значениями регистров
+            XCHG eax, ebx
+            mov dword ptr[x], eax
+            mov dword ptr[y], ebx
+        }
+        std::cout << "<x,y>=" << "<" << x << "," << y << ">" << std::endl;
+    }
+    /*Test3*/else if (false)
+    {
+        int x = 10;int y = 11;
+        std::cout << "<x,y>=" << "<" << x << "," << y << ">" << std::endl;
+        __asm
+        {
+            xor ebx, ebx
+            mov ebx, dword ptr[x]
+            XCHG ebx, dword ptr[y]
+            mov dword ptr[x], ebx
+        }
+        std::cout << "<x,y>=" << "<" << x << "," << y << ">" << std::endl;
+    }
+    /*Test4*/else if (false)
+    {
+        std::cout << "Test_3();" << std::endl;
+        int x = 10;int y = 11;
+        std::cout << "<x,y>=" << "<" << x << "," << y << ">" << std::endl;
+        __asm
+        {
+            xor ebx, ebx
+            mov ebx, x
+            XCHG ebx, y
+            mov x, ebx
+        }
+        std::cout << "<x,y>=" << "<" << x << "," << y << ">" << std::endl;
+    }
+    /*TimeTest1*/{
+        int x = 10;int y = 11;
+        __Set_TestTimeStart
+            __asm
+        {
+            //Обнуление регистра на пожарный случай
+            xor eax, eax
+            xor ebx, ebx
+            //Команда пересылки данных. eax = x
+            mov eax, dword ptr[x]
+            mov ebx, dword ptr[y]
+            //Команда пересылки данных. x=eax
+            mov dword ptr[x], eax
+            mov dword ptr[y], ebx
+        }
+        __Set_TestTimeStop
+    }
+    /*TimeTest2*/{
+        int x = 10;int y = 11;
+        __Set_TestTimeStart
+        __asm
+        {
+            xor eax, eax
+            xor ebx, ebx
+            mov eax, dword ptr[x]
+            mov ebx, dword ptr[y]
+            //Обмен значениями регистров
+            XCHG eax, ebx
+            mov dword ptr[x], eax
+            mov dword ptr[y], ebx
+        }
+        __Set_TestTimeStop
+    }
+    /*TimeTest3*/{
+        int x = 10;int y = 11;
+        __Set_TestTimeStart
+        __asm
+        {
+            xor ebx, ebx
+            mov ebx, dword ptr[x]
+            XCHG ebx, dword ptr[y]
+            mov dword ptr[x], ebx
+        }
+        __Set_TestTimeStop
+    }
+    /*TimeTest4*/{
+        int x = 10;int y = 11;
+        __Set_TestTimeStart
+        __asm
+        {
+            xor ebx, ebx
+            mov ebx, x
+            XCHG ebx, y
+            mov x, ebx
+        }
+        __Set_TestTimeStop
+    }
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
